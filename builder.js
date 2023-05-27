b7a35d1642efefcaa9960c296d9bb28899b7a8e4
Sarmaged <sarmaged@gmail.com>
2023-05-27 14:26:38 +0300

    Changed repository & Fix builder.js

--- builder.js
+++ builder.js
@@ -3,15 +3,14 @@ const postcss = require('postcss')
 const atImport = require('postcss-import')
 const autoprefixer = require('autoprefixer')
 
-const package = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-
-const title = package.name + ' v' + package.version
-const license = package.license + ' License'
-const link = 'github.com/' + package.repository.split(':')[1].slice(0, -4)
-const header = '/*! ' + [title, license, link].join(' | ') + ' */'
-
 const [from, to] = ['src/all.css', 'base.css']
 const css = fs.readFileSync(from, 'utf8')
+
+const package = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+const title = package.name + ' v' + package.version
+const license = package.license + ' License'
+const link = 'github.com/' + package.repository.split(':')[1]
+const header = '/*! ' + [title, license, link].join(' | ') + ' */'
 
 postcss([atImport, autoprefixer])
   .process(css, { from })
